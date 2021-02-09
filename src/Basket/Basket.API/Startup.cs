9b8ba6a1f8f82d7c9b6fdf85165652d87c46565e
Robin Bette <robinbette@hotmail.com>
2021-02-09 10:05:45 +0100

    Update controller

--- src/Basket/Basket.API/Startup.cs
+++ src/Basket/Basket.API/Startup.cs
@@ -33,7 +33,7 @@ namespace Basket.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket API", Version = "v1" });
             });
 
             services.AddSingleton(c =>
@@ -55,7 +55,7 @@ namespace Basket.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket API v1"));
             }
 
             app.UseRouting();
