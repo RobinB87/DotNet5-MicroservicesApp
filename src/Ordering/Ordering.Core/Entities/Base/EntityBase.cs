34bd6a537f6e87bc309bcf76910a89e7dd40fb6b
Robin Bette <robinbette@hotmail.com>
2021-02-09 15:58:24 +0100

    Add T Repository and Entity

---
+++ src/Ordering/Ordering.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,66 @@
+﻿namespace Ordering.Core.Entities.Base
+{
+    public abstract class EntityBase<TId> : IEntityBase<TId>
+    {
+        // use this approach so you can change db provider 
+        // (eg mongo db, PK = string, sql = int)
+        // 
+        // virtual, so you can override the id
+        // protected so you can set the id in the base classes
+        public virtual TId Id { get; protected set; }
+
+        int? _requestedHashCode;
+
+        public bool IsTransient()
+        {
+            return Id.Equals(default(TId));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is EntityBase<TId>))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var item = (EntityBase<TId>)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+            else
+                return item == this;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                // XOR for random distribution 
+                // (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)// XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = Id.GetHashCode() ^ 31; 
+
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+
+        public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            if (Equals(left, null))
+                return Equals(right, null) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            return !(left == right);
+        }
+    }
+}
