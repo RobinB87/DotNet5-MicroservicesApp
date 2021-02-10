dcb49b1631ff8fe633689b240db5f4dc05273148
Robin Bette <robinbette@hotmail.com>
2021-02-10 16:22:36 +0100

    Fix index page

--- src/WebApp/AspnetRunBasics/Pages/Index.cshtml.cs
+++ src/WebApp/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.ApiCollection.Interfaces;
+using WebApp.Models;
 
 namespace WebApp.Pages
 {
@@ -18,20 +19,30 @@ namespace WebApp.Pages
             _basketApi = basketApi ?? throw new ArgumentNullException(nameof(basketApi));
         }
 
-        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+        public IEnumerable<Catalog> ProductList { get; set; } = new List<Catalog>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            ProductList = await _productRepository.GetProducts();
+            ProductList = await _catalogApi.GetCatalog();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+            var product = await _catalogApi.GetCatalog(productId);
+            var userName = "Robin";
+            var basket = await _basketApi.GetBasket(userName);
 
-            await _cartRepository.AddItem("test", productId);
+            basket.Items.Add(new BasketItem
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            var basketUpdated = await _basketApi.UpdateBasket(basket);
             return RedirectToPage("Cart");
         }
     }
